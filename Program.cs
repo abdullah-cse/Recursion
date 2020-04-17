cf85b4720eb80efd3a72a3c7d429a2a4cba8ff11
MD ABDULLAH <abdullah_bd333@outlook.com>
2020-04-17 21:40:04 +0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursion
+{
+    class Program
+    {
+        public static int Fact(int number)
+        {
+            if (number == 1)
+            {
+                return 1;
+            }
+            return number * Fact(number - 1);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a value to calculate Factorial");
+            Console.WriteLine(Fact(Convert.ToInt32(Console.ReadLine())));
+            Console.ReadLine();
+        }
+    }
+}
